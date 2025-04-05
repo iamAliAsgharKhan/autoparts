f8e4f51a646d62343b9644b5c48141bb19915d26
iamaliasgharkhan <aliasgharforwork@gmail.com>
2025-04-05 15:58:47 +0500

    modified years api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -134,19 +134,12 @@ class HomeController extends Controller
 
     public function apiyears(Request $request)
     {
-        $modelId = $request->query('model_id');
-
-        if (!$modelId) {
-            return response()->json(['error' => 'model_id is required'], 400);
-        }
-
-        // Get distinct years associated with this model through existing parts
-        $years = Year::whereHas('parts', function($query) use ($modelId) {
-            $query->where('car_model_id', $modelId);
-        })->distinct()->get(['id', 'year']);
-
+        // Retrieve all available years
+        $years = Year::all(['id', 'year']);
+    
         return response()->json($years);
     }
+    
 
     public function publicShow(Project $project)
     {
